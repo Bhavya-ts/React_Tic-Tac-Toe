450cf9ded6b6e89bd6323dbabaa5b0ee1be5b159
Bhavya Bhagat <bhavyabhagat@BhavyaBhagat-MacBook-Pro.local>
2024-08-07 10:30:08 +0530

    Ui part for tic tec toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import userEvent from "@testing-library/user-event";
+import React, { useState } from "react";
 
 function App() {
+  const value = "-";
+  const [sign, setSign] = useState(true);
+  const [metrix, setMetrix] = useState([
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ]);
+  function winLogic(col, row, sign, co) {
+    let count = 0;
+
+    // if (count == 4 || row == -1 || col == -1 || row == 3 || col == 3) {
+    //   return;
+    // }
+    // if (count == 3 && metrix[row][col] == sign) {
+    //   console.log("we have a winner ...", sign);
+    //   return;
+    // }
+    // if (metrix[row][col] == sign) {
+    //   winLogic(col - 1, row, sign, count++);
+    //   winLogic(col + 1, row, sign, count++);
+    //   winLogic(col, row - 1, sign, count++);
+    //   winLogic(col, row + 1, sign, count++);
+    //   winLogic(col - 1, row - 1, sign, count++);
+    //   winLogic(col - 1, row + 1, sign, count++);
+    //   winLogic(col + 1, row - 1, sign, count++);
+    //   winLogic(col + 1, row + 1, sign, count++);
+    // }
+    return;
+  }
+  function onClickHandler(row, col) {
+    console.log("called");
+    let copy = [...metrix];
+    if (copy[row][col] != "-") {
+      return;
+    }
+    if (sign == true) {
+      copy[row][col] = "O";
+      //   winLogic(col, row, "O", 1);
+    } else {
+      copy[row][col] = "X";
+      //   winLogic(col, row, "X", 1);
+    }
+    //
+    setMetrix(copy);
+
+    setSign(!sign);
+
+    // console.log(metrix);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="status">
+        <h1>tic toc</h1>
+        {metrix.map((row, rowIndex) => {
+          // console.log("row = ", rowIndex);
+          return (
+            <div className="raw">
+              {row.map((col, colINdex) => {
+                // console.log("col = ", colINdex);
+                return (
+                  <button
+                    id={colINdex}
+                    onClick={() => onClickHandler(rowIndex, colINdex)}
+                  >
+                    {col}
+                  </button>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
-
 export default App;
